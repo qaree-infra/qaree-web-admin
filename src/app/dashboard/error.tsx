23c3a00d90ad080fdca3cd3549916a7f7f9ec6e7
mohamedtsx <mohamed.tsx@gmail.com>
2024-04-26 11:23:43 +0300

    feat: better error page messages

--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -17,11 +17,20 @@ export default function ErrorPage({
 					<p className="mb-4 text-lg font-semibold text-primary-sky">
 						There was a problem
 					</p>
-					<h1 className="mb-10 max-w-prose text-balance text-3xl font-bold ">
+					<h1 className="mb-10 max-w-prose text-balance text-2xl font-bold text-destructive capitalize sm:text-5xl ">
 						{error.message || "Something went wrong"}
 					</h1>
 					<p className="opacity-80">
-						Please try again later or contact support if the problem persistes.
+						Please check your internet connection or contact{" "}
+						<a
+							rel="noreferrer"
+							href="mailto:ma0068664@gmail.com?subject=Qaree admin dashboard problem"
+							target="_blank"
+							className="underline hover:text-muted-foreground transition"
+						>
+							support
+						</a>{" "}
+						if the problem persistes.
 					</p>
 				</div>
 				<div className="flex flex-wrap justify-center gap-5 sm:gap-16">
