d21fff938a7c7e7c7b114f4c1d0d1ddab9ae7798
mohamedtsx <mohamed.tsx@gmail.com>
2024-04-29 06:04:56 +0300

    feat: add offers columns

--- src/app/dashboard/offeres/columns.tsx
+++ src/app/dashboard/offeres/columns.tsx
@@ -1,1 +1,60 @@
-interface Offer {}
+import { buttonVariants } from "@/components/ui/button";
+import type { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+
+export interface Offer {
+	_id: string;
+	percent: number;
+	expireAt: string;
+	book: {
+		_id: string;
+		name: string;
+	};
+	createdAt: string;
+	updatedAt: string;
+}
+
+export const columns: ColumnDef<Offer>[] = [
+	{
+		accessorKey: "percent",
+		header: "Percent",
+	},
+	{
+		accessorKey: "createdAt",
+		header: "Created At",
+	},
+	{
+		accessorKey: "updatedAt",
+		header: "Last Edit",
+	},
+	{
+		accessorKey: "expireAt",
+		header: "Expire Date",
+	},
+	{
+		accessorKey: "book",
+		header: "Book",
+		cell({ row }) {
+			const book = row.original.book;
+			return (
+				<div>
+					<Link
+						className={buttonVariants({
+							variant: "link",
+						})}
+						href={`/dashboard/books/${book._id}`}
+					>
+						@{book.name}
+					</Link>
+				</div>
+			);
+		},
+	},
+	{
+		id: "actions",
+		header: "Actions",
+		cell(props) {
+			return <div>edit & delete</div>;
+		},
+	},
+];
