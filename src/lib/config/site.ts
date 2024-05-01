66199fd28b7ebc8134f210e85df88140c089eabc
mohamedtsx <mohamed.tsx@gmail.com>
2024-05-01 08:18:51 +0300

    chore: starter contact message

---
+++ src/lib/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+	name: "Qaree",
+	title: "Qaree Admin Dashboard",
+	description:
+		"Manage and monitor Qaree e-book reading app through the user-friendly Qaree Admin Dashboard",
+	links: {
+		whatsapp: "https://wa.me/+201094755628?text=السلام عليكم 👋",
+		github: "https://github.com/qaree-infra",
+		url: "",
+	},
+};
