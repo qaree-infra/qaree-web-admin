5390b0e16b6c04cd79555c30cb1267fe4ed143dc
mohamedtsx <mohamed.tsx@gmail.com>
2024-05-01 10:10:23 +0300

    fix: undo auth options updates

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -4,8 +4,10 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { redirect } from "next/navigation";
 import { fetcher } from "./graphql/fetcher";
 import { signInMutation } from "./graphql/mutations";
+import { getAdminInfoQuery } from "./graphql/queries";
 import type { AuthUser } from "./graphql/types";
 
+// TODO: remove the placeholder data
 export const authOptions: NextAuthOptions = {
 	providers: [
 		CredentialsProvider({
@@ -20,9 +22,34 @@ export const authOptions: NextAuthOptions = {
 					type: "password",
 				},
 			},
-			async authorize(credentials) {
+			async authorize(credentials, req) {
 				const email = credentials?.email as string;
 				const password = credentials?.password as string;
+				const resetToken = req.body?.reset_token;
+
+				if (resetToken) {
+					// todo(security) validate the token
+
+					// // I need more data about user
+					const { getAdminInfo } = await fetcher({
+						query: getAdminInfoQuery,
+						server: true,
+						protectid: true,
+						headers: {
+							Authorization: `Bearer ${resetToken}`,
+						},
+						cache: "default",
+					});
+
+					return {
+						access_token: resetToken,
+						name: getAdminInfo?.name as string,
+						email: getAdminInfo?.email as string,
+						avatar: getAdminInfo?.avatar as {
+							path: string;
+						},
+					};
+				}
 
 				if (!email || !password) return null;
 
@@ -34,12 +61,29 @@ export const authOptions: NextAuthOptions = {
 					},
 					server: true,
 					protectid: false,
+					cache: "default",
 				});
 
 				if (!signIn?.access_token) return null;
 
+				// I need more data about user
+				const { getAdminInfo } = await fetcher({
+					query: getAdminInfoQuery,
+					server: true,
+					protectid: false,
+					headers: {
+						Authorization: `Bearer ${signIn.access_token}`,
+					},
+					cache: "no-cache",
+				});
+
 				const user = {
 					access_token: signIn.access_token,
+					name: getAdminInfo?.name as string,
+					email: getAdminInfo?.email as string,
+					avatar: getAdminInfo?.avatar as {
+						path: string;
+					},
 				};
 
 				return user;
@@ -52,7 +96,10 @@ export const authOptions: NextAuthOptions = {
 				return { ...token, ...session.user };
 			}
 			if (user) {
+				token.name = user.name;
+				token.email = user.email;
 				token.access_token = user.access_token;
+				token.avatar = user.avatar;
 			}
 
 			return { ...token, ...user };
@@ -78,8 +125,13 @@ export async function getCurrentUser() {
 }
 
 declare module "next-auth" {
-	interface User {
-		access_token: string;
+	interface User extends AuthUser {
 		id?: string;
 	}
+	interface Session {
+		user: AuthUser;
+	}
+}
+declare module "next-auth/jwt" {
+	interface JWT extends AuthUser {}
 }
